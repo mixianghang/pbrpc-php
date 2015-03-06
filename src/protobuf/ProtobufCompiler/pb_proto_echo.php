91044f552ef0d1756046253cc01be5f4029614c1
mixianghang <mxh.scott@gmail.com>
2015-03-06 20:17:13 +0800

    xiufu

---
+++ src/protobuf/ProtobufCompiler/pb_proto_echo.php
@@ -0,0 +1,245 @@
+<?php
+/**
+ * Auto generated from echo.proto at 2015-03-06 20:06:29
+ *
+ * pack.test package
+ */
+
+/**
+ * Nested_Message message embedded in EchoRequest message
+ */
+class Ugc_Pb_Pack_Test_EchoRequest_NestedMessage extends ProtobufMessage
+{
+    /* Field index constants */
+    const MESSAGE = 1;
+
+    /* @var array Field descriptors */
+    protected static $fields = array(
+        self::MESSAGE => array(
+            'name' => 'message',
+            'required' => false,
+            'type' => 7,
+        ),
+    );
+
+    /**
+     * Constructs new message container and clears its internal state
+     *
+     * @return null
+     */
+    public function __construct()
+    {
+        $this->reset();
+    }
+
+    /**
+     * Clears message values and sets default ones
+     *
+     * @return null
+     */
+    public function reset()
+    {
+        $this->values[self::MESSAGE] = null;
+    }
+
+    /**
+     * Returns field descriptors
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return self::$fields;
+    }
+
+    /**
+     * Sets value of 'message' property
+     *
+     * @param string $value Property value
+     *
+     * @return null
+     */
+    public function setMessage($value)
+    {
+        return $this->set(self::MESSAGE, $value);
+    }
+
+    /**
+     * Returns value of 'message' property
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->get(self::MESSAGE);
+    }
+}
+
+/**
+ * EchoRequest message
+ */
+class Ugc_Pb_Pack_Test_EchoRequest extends ProtobufMessage
+{
+    /* Field index constants */
+    const MESSAGE = 1;
+    const NESTED_MESSAGE = 2;
+
+    /* @var array Field descriptors */
+    protected static $fields = array(
+        self::MESSAGE => array(
+            'name' => 'message',
+            'required' => true,
+            'type' => 7,
+        ),
+        self::NESTED_MESSAGE => array(
+            'name' => 'nested_message',
+            'required' => false,
+            'type' => 'Ugc_Pb_Pack_Test_EchoRequest_NestedMessage'
+        ),
+    );
+
+    /**
+     * Constructs new message container and clears its internal state
+     *
+     * @return null
+     */
+    public function __construct()
+    {
+        $this->reset();
+    }
+
+    /**
+     * Clears message values and sets default ones
+     *
+     * @return null
+     */
+    public function reset()
+    {
+        $this->values[self::MESSAGE] = null;
+        $this->values[self::NESTED_MESSAGE] = null;
+    }
+
+    /**
+     * Returns field descriptors
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return self::$fields;
+    }
+
+    /**
+     * Sets value of 'message' property
+     *
+     * @param string $value Property value
+     *
+     * @return null
+     */
+    public function setMessage($value)
+    {
+        return $this->set(self::MESSAGE, $value);
+    }
+
+    /**
+     * Returns value of 'message' property
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->get(self::MESSAGE);
+    }
+
+    /**
+     * Sets value of 'nested_message' property
+     *
+     * @param Ugc_Pb_Pack_Test_EchoRequest_NestedMessage $value Property value
+     *
+     * @return null
+     */
+    public function setNestedMessage(Ugc_Pb_Pack_Test_EchoRequest_NestedMessage $value)
+    {
+        return $this->set(self::NESTED_MESSAGE, $value);
+    }
+
+    /**
+     * Returns value of 'nested_message' property
+     *
+     * @return Ugc_Pb_Pack_Test_EchoRequest_NestedMessage
+     */
+    public function getNestedMessage()
+    {
+        return $this->get(self::NESTED_MESSAGE);
+    }
+}
+
+/**
+ * EchoResponse message
+ */
+class Ugc_Pb_Pack_Test_EchoResponse extends ProtobufMessage
+{
+    /* Field index constants */
+    const MESSAGE = 1;
+
+    /* @var array Field descriptors */
+    protected static $fields = array(
+        self::MESSAGE => array(
+            'name' => 'message',
+            'required' => true,
+            'type' => 7,
+        ),
+    );
+
+    /**
+     * Constructs new message container and clears its internal state
+     *
+     * @return null
+     */
+    public function __construct()
+    {
+        $this->reset();
+    }
+
+    /**
+     * Clears message values and sets default ones
+     *
+     * @return null
+     */
+    public function reset()
+    {
+        $this->values[self::MESSAGE] = null;
+    }
+
+    /**
+     * Returns field descriptors
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return self::$fields;
+    }
+
+    /**
+     * Sets value of 'message' property
+     *
+     * @param string $value Property value
+     *
+     * @return null
+     */
+    public function setMessage($value)
+    {
+        return $this->set(self::MESSAGE, $value);
+    }
+
+    /**
+     * Returns value of 'message' property
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->get(self::MESSAGE);
+    }
+}
---
+++ src/protobuf/ProtobufCompiler/protoc-php.php
@@ -0,0 +1,62 @@
+<?php
+require 'ProtobufParser.php';
+
+if (!debug_backtrace()) {
+    if (!class_exists('ProtobufMessage')) {
+
+        echo $argv[0] .
+            ' requires protobuf extension installed to run' .
+            PHP_EOL;
+
+        exit(1);
+    }
+
+    $optionError = false;
+    $useNamespaces = false;
+
+    $iterator = new RegexIterator(new ArrayIterator($argv), '/^-/');
+    foreach ($iterator as $key => $value) {
+        switch ($value) {
+            case '-n' :
+            case '--use-namespaces' :
+                $useNamespaces = true;
+                break;
+            case '--prefix' :
+            case '-p' :
+                $prefix = $argv[$key+1];
+                break;
+            case '--outdir' :
+            case '-o' :
+                $outdir = $argv[$key+1];
+                break;
+            default :
+                $optionError = true;
+                break;
+        }
+    }
+
+    if ($optionError) {
+        printf('USAGE: %s [OPTIONS] PROTO_FILE' . PHP_EOL, $argv[0]);
+        printf('  -n, --use-namespaces      Use native PHP namespaces' . PHP_EOL);
+        exit(1);
+    }
+
+    $parser = new ProtobufParser($useNamespaces,$prefix,$outdir);
+
+    $file = $argv[1];
+    if (!file_exists($file)) {
+        printf($file . ' does not exist' . PHP_EOL);
+        exit(1);
+    }
+
+    if (!is_file($file)) {
+        printf($file . ' is not a file' . PHP_EOL);
+        exit(1);
+    }
+
+    try {
+        $parser->parse($file,$outdir);
+    } catch (Exception $e) {
+        echo $e->getMessage() . PHP_EOL;
+    }
+}
